cf454322125cfa68df2398098a8d84a61a2494aa
shashank singh chandel <shashakchandel@gmail.com>
2020-10-25 00:37:50 +0530

    ver-1

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'react-thunk';
+import reducer from './reducers';
+
+let store;
+export function configureStore() {
+  store = createStore(reducer, applyMiddleware(thunk));
+  return store;
+}
