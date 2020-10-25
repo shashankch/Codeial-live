56134174eebb6f8cb941835770db8a6e51ea78bb
shashank singh chandel <shashakchandel@gmail.com>
2020-10-25 22:26:47 +0530

    added uncontrolled form handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Link, Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 import { fetchPosts } from '../actions/posts';
-import { Home, Navbar,Page404 } from './';
+import { Home, Navbar,Page404,Login } from './';
 import PropTypes from 'prop-types';
 
-const Login = () => <div>Login</div>;
+//const Login = () => <div>Login</div>;
 
 const Signup = () => <div>Signup</div>;
 
-// const Home = (props) => {
-//   console.log(props);
-//   return <div>Home</div>;
-// };
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -24,18 +20,7 @@ class App extends React.Component {
       <Router>
         <div>
           <Navbar />
-          {/* <PostsList posts={posts} /> */}
-          {/* <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul> */}
+          
           <Switch>
           <Route
             exact
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Navbar from './Navbar';
 import App from './App';
 import Home from './Home';
 import Page404 from './Page404';
+import Login from './Login';
 
-export { Navbar, App,Home,PostsList,Page404 };
+export { Navbar, App,Home,PostsList,Page404,Login };
