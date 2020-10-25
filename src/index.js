54e2ffed4f9e314e254a8b7247c58cff152dd34a
shashank singh chandel <shashakchandel@gmail.com>
2020-10-25 16:29:02 +0530

    added navbar and posts list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import { configureStore } from './store';
+
+const store = configureStore();
+console.log('store', store);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
-
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'react-thunk';
-import reducer from './reducers';
+import thunk from 'redux-thunk';
+import {logger} from 'redux-logger';
+import reducer from '../reducers';
 
 let store;
 export function configureStore() {
-  store = createStore(reducer, applyMiddleware(thunk));
+  store = createStore(reducer, applyMiddleware(thunk,logger));
   return store;
 }
